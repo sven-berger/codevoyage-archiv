3fecc26c12f00097b9fca5b5bdc69a6cb04d276f
Sven Oliver Berger <bergersvenoliver@gmail.com>
2025-08-19 13:49:12 +0200

    Delete 1.0.0/python/config.py

--- 1.0.0/python/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'codevoyage_system' 
-    MYSQL_PASSWORD = 'jD=0m7!I'
-    MYSQL_DB = 'codevoyage_system'
